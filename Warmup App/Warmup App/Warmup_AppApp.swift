70d678690b55c5f09e4f3e2918af0a17cd69af39
timgivesgame <timgivesgame@gmail.com>
2025-02-14 10:55:50 -0500

    Added Xcode project

---
+++ Warmup App/Warmup App/Warmup_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Warmup_AppApp.swift
+//  Warmup App
+//
+//  Created by TK on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Warmup_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Warmup App/Warmup AppTests/Warmup_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Warmup_AppTests.swift
+//  Warmup AppTests
+//
+//  Created by TK on 2/14/25.
+//
+
+import Testing
+
+struct Warmup_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
